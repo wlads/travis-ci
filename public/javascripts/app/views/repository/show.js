1a10fdd148fd4d141dc9553750508549d9877cad
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2011-08-20 22:23:57 +0200

    Adding default tabs to build show.

--- public/javascripts/app/views/repository/show.js
+++ public/javascripts/app/views/repository/show.js
@@ -9,22 +9,24 @@ Travis.Views.Repository.Show = Backbone.View.extend({
   render: function() {
     this.el = $(this.template({}));
     this.setTitle();
+
+    _.each(this.tab_names, _.bind(function(tab_name){
+      this.renderTab(tab_name)
+    }, this));
     return this;
   },
   activateTab: function(name) {
     _.each(this.tabs, function(tab) { if(tab.name != name) tab.deactivate(); })
     this.tabs[name].activate();
   },
+  // Somewhat ambiguous functions
   selectTab: function(name) {
-    this.createTab(name)
-    this.renderTab(name)
-    this.activateTab(name)
-  },
-  createTab: function(name) {
-    this.tabs[name] = new Travis.Views.Repository.Tab({ name: name, parent: this, model: this.model });
+    this.tabs[name].activate()
   },
   renderTab: function(name) {
+    this.tabs[name] = new Travis.Views.Repository.Tab({ name: name, parent: this, model: this.model });
     this.el.find('.tabs').append(this.tabs[name].render().el);
+    return this.tabs[name]
   },
   setTitle: function() {
     this.el.find('h3 a:first-child').attr('href', 'http://github.com/' + this.model.get('slug')).text(this.model.get('slug'));
