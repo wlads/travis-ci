a94b6f4ca4127ba10ff553e7603c19fd62dbb875
Sven Fuchs <me@svenfuchs.com>
2012-09-17 12:52:31 +0200

    this would find the user service, not the model

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,7 +2,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   include SyncHelper
 
   def github
-    user = User.find_or_create_for_oauth(env["omniauth.auth"])
+    user = ::User.find_or_create_for_oauth(env["omniauth.auth"])
 
     if user.persisted?
       flash[:notice] = I18n.t("devise.omniauth_callbacks.success", kind: "GitHub")
